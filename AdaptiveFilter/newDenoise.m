b374342933d63caaecc0826654c2d24b7f5cde4c
Yuming Chai <ymchai@mail.ustc.edu.cn>
2024-01-17 22:46:53 +0800

    Optimizing denoising methods and adding signal processing for multicellular ensembles at low spatial resolution.

--- AdaptiveFilter/newDenoise.m
+++ AdaptiveFilter/newDenoise.m
@@ -1,6 +1,6 @@
 function nsig = newDenoise(sig)
 
-nsig=sig;
+nsig1=sig;
 
 sDiff=diff(sig);
 
@@ -8,8 +8,18 @@ le=length(sig);
 
 for ii=2:le-1
     if sign(sDiff(ii-1))~=sign(sDiff(ii)) && abs(sig(ii+1)-sig(ii-1))<abs(sDiff(ii-1))/3
-        nsig(ii)=(sig(ii-1)+sig(ii+1))/2;
+        nsig1(ii)=(sig(ii-1)+sig(ii+1))/2;
     end
+end
+
+DFoP=diff(nsig1)./nsig1(1:le-1);
+AT=find(DFoP>0.08);
+AT(AT<3)=[];
+AT(AT>le-4)=[];
+nsig = smoothdata(nsig1,'gaussian',10);
+le2=length(AT);
+for jj=1:le2
+    nsig(AT(jj)-2:AT(jj)+4)=nsig1(AT(jj)-2:AT(jj)+4);
 end
 
 end
--- AdaptiveFilter/useNLMS_CellGroup.m
+++ AdaptiveFilter/useNLMS_CellGroup.m
@@ -1,29 +1,34 @@
-function [AF, e, y]=useNLMS(sigR,sigG, extendfactor)
+function [AF, es6, y]=useNLMS_CellGroup(sigR,sigG, extendfactor)
 
 sigG = newDenoise(sigG);
+
 sigR = newDenoise(sigR);
+
 % extend the signal
 L = length(sigG);
 
-MM = mean(sigG);
+MM = prctile(sigG,10);
 prefixSig(1:ceil(L*extendfactor)) = MM;
 prefixSig = awgn(prefixSig,1);
 sigG = [prefixSig sigG];
 
-MM1 = mean(sigR);
+MM1 = prctile(sigR,10);
 prefixSig1(1:ceil(L*extendfactor)) = MM1;
 prefixSig1 = awgn(prefixSig1,1);
 sigR = [prefixSig1 sigR];
 
 
-[e,y,~] = myNLMS(sigR, sigG, 0.8, 2, -1e-3, -1e-4);
+[e,y,~] = myNLMS_t(sigR, sigG, 0.5, 2, -1e-3, -1e-4);
 
-e = thresholdDenoise3(e, mean(sigG)*0.1,0.05);
+% % % e = thresholdDenoise3(e, mean(sigG)*0.1,0.05);
 e = e(ceil(L*extendfactor)+1:end);
 y = y(ceil(L*extendfactor)+1:end);
-e = e./y;
+y(y==0) = sigG(y==0);
+es6 = movsum(e,6);
+es6 = thresholdDenoise3(es6,mean(sigG)*0.4,0.05);
+es6 = es6./y;
 
 ARKernel = generateAR1(0.95);
-AF = spikeConvolution(e, ARKernel);
+AF = spikeConvolution(es6/8, ARKernel);
 
 end
